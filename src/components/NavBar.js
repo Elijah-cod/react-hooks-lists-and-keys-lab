5dd238ec9f3989136f42918c25d1b08a7abb4fec
Elijah <iamngunjiri@gmail.com>
2025-01-16 14:45:10 +0300

    Complete Navbar test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map((link, i)=>{
+    return <a key={i+1} href={'#'+link}>{link}</a>
+  })
+  return <nav>
+    <ul>
+      {navLinks}
+    </ul>
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,19 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  for (let project of projects) {
+    console.log(project)
+  }
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {
+          projects.forEach((elem)=>{
+            <ProjectItem key={elem.id} name={elem.name} about={elem.about} technologies={elem.technologies} />
+          })
+        }
+      </div>
     </div>
   );
 }
