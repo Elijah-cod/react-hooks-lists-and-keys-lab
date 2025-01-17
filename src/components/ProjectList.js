fd77d41ac0f981ebc3d1edb11883418b02e55f15
Elijah <iamngunjiri@gmail.com>
2025-01-17 11:07:54 +0300

    Complete Lists and Keys Lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,17 +2,15 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  for (let project of projects) {
-    console.log(project)
-  }
+  console.log(projects)
   return (
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">
         {
-          projects.forEach((elem)=>{
+          projects.map((elem)=>
             <ProjectItem key={elem.id} name={elem.name} about={elem.about} technologies={elem.technologies} />
-          })
+          )
         }
       </div>
     </div>
